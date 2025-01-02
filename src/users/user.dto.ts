933d1f34feb567c26f58f69941fbe4ef374b5ce3
Brayan Muzzi <bcmuzzi@hotmail.com>
2025-01-01 21:57:40 -0300

    new dto validators and services error handling

--- src/users/user.dto.ts
+++ src/users/user.dto.ts
@@ -1,20 +1,26 @@
 import { ApiProperty } from '@nestjs/swagger';
-import { IsString, Min, Max, IsNotEmpty } from 'class-validator';
+import {
+  IsString,
+  IsNotEmpty,
+  IsOptional,
+  MinLength,
+  MaxLength,
+} from 'class-validator';
 
 export class UserDto {
   @ApiProperty()
-  @IsString()
-  id: string;
+  @IsOptional()
+  @IsString({ message: 'ID must be a valid string.' })
+  id?: string;
 
   @ApiProperty()
-  @IsString()
-  @IsNotEmpty({ message: 'Username should not be empty' })
+  @IsString({ message: 'Username must be a string.' })
+  @IsNotEmpty({ message: 'Username is required.' })
   username: string;
 
   @ApiProperty()
   @IsString()
-  @IsNotEmpty({ message: 'Password should not be empty' })
-  @Min(8)
-  @Max(24)
+  @MinLength(8, { message: 'Password must be at least 8 characters long.' })
+  @MaxLength(24, { message: 'Password cannot exceed 24 characters.' })
   password: string;
 }
