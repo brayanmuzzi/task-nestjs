8f96d1e54ea3b9bff25da7363660de8c59619240
Brayan Muzzi <bcmuzzi@hotmail.com>
2025-01-03 17:38:02 -0300

    users controller swagger/documentation updates

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,12 +1,20 @@
 import { Body, Controller, Post } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { UserDto } from './user.dto';
+import {
+  ApiOperation,
+  ApiOkResponse,
+  ApiNotFoundResponse,
+} from '@nestjs/swagger';
 
 @Controller('users')
 export class UsersController {
   constructor(private readonly usersService: UsersService) {}
 
   @Post()
+  @ApiOperation({ summary: 'Create user in database' })
+  @ApiOkResponse({ description: 'User was created.' })
+  @ApiNotFoundResponse({ description: 'User cannot be created.' })
   async create(@Body() user: UserDto) {
     this.usersService.create(user);
   }
